e7ca2ef4aea4e58e136c0d4d5518f24121bd35b5
Bram <bram.moons@i8c.be>
2020-11-22 14:01:10 +0100

    datasheet

--- Website/finalwork_brammoons_frontend/src/AllWalks.js
+++ Website/finalwork_brammoons_frontend/src/AllWalks.js
@@ -1,12 +1,64 @@
 import React from 'react';
 import axios from 'axios';
+import DataSheet from 'react-datasheet';
 
-function AllWalks() {
-    return(
+class AllWalks extends React.Component {
+
+    Constructor(props) {
+        super(props);
+        this.state = {
+            grid: [
+                [
+                    {readOnly: true},
+                    {value: ''},
+                    {value: ''},
+                    {value: ''},
+                    {value: ''},
+                ],
+                [
+                    {readOnly: true},
+                    {value: 1},
+                    {value: 2},
+                    {value: 3},
+                    {value: 4}
+                ],
+                [
+                    {readOnly: true},
+                    {value: 1},
+                    {value: 2},
+                    {value: 3},
+                    {value: 4}
+                ]
+            ]
+        }
+    }
+
+    valueRenderer = cell => cell.value;
+
+    onCellsChanged = changes => {
+        const grid = this.state.grid;
+        changes.forEach(({ cell, row, col, value }) => {
+            grid[row][col] = { ...grid[row][col], value };
+        });
+        this.setState=({ grid });
+    };
+
+    onCentextMenu = (e, cell, i, j) =>
+        cell.readOnly ? e.preventDefault() : null;
+
+    render() {
+        return(
             <div id="AllWalks">
                 <h1>Function All walks test page</h1>
+                <DataSheet
+                    data={this.state.grid}
+                    valueRenderer={this.valueRenderer}
+                    onContextMenu={this.onContextMenu}
+                    onCellsChanged={this.onCellsChanged}
+                />
             </div>
-    );
+        );
+    }
 }
 
 export default AllWalks;
