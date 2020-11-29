ac8bf6e04e3fcc1e9cdc388168d946eabe588527
Bram <bram.moons@i8c.be>
2020-11-29 15:11:44 +0100

    animal and walk update

--- Website/finalwork_brammoons_frontend/src/AddAnimal.js
+++ Website/finalwork_brammoons_frontend/src/AddAnimal.js
@@ -1,12 +1,85 @@
 import React from 'react';
 import axios from 'axios';
 
-function AddAnimal() {
-    return(
-        <div id="AddAnimal">
-               <h1>Function Add animal page test</h1>
-        </div>
-    );
+class AddAnimal extends React.Component {
+
+    constructor() {
+        let Dier = {};
+        dier.ras = "";
+        dier.grootte = "";
+        dier.kleur = "";
+        dier.karakter = "";
+        dier.geslacht = "";
+        dier.leeftijd = "";
+        dier.foto = "";
+        this.state = { dier:{naam: "Jef", ras: "witte herder", grootte: 40, kleur: "wit", karakter: "sociaal", geslacht: "man", leeftijd: 1, foto: "WitteHerder.jpg"} }
+    }
+
+    handleSubmit() {
+        axios.put("http://127.0.0.1:8080/Dier/voegtoe?dier=" + this.dier);
+    }
+
+    handleChange(event) {
+        let dier = this.state.dier;
+        switch(event.target.id){
+            case "name":
+                dier.naam = event.target.value;
+                break;
+            case "ras":
+                dier.ras = event.target.value;
+                break;
+            case "grootte":
+                dier.grootte = event.target.value;
+                break;
+            case "kleur":
+                dier.grootte = event.target.value;
+                break;
+            case "karakter":
+                dier.karakter = event.target.value;
+                break;
+            case "geslacht":
+                dier.geslacht = event.target.value;
+                break;
+            case "leeftijd":
+                dier.leeftijd = event.target.value;
+                break;
+            case "foto":
+                dier.foto = event.target.value;
+                break;
+            default:
+                //can be ignored;
+        }
+
+        this.setState(dier);
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Voeg dier toe</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Naam van het dier</label><br></br>
+                    <input type="text" id="name" onChange={this.handleChange} value={this.state.dier.naam}></input><br></br>
+                    <label>Ras van het dier</label><br></br>
+                    <input type="text" id="ras" onChange={this.handleChange} value={this.state.dier.ras}></input><br></br>
+                    <label>Grootte van het dier</label><br></br>
+                    <input type="number" id="grootte" onChange={this.handleChange} value={this.state.dier.grootte}></input><br></br> 
+                    <label>Kleur van het dier</label><br></br>
+                    <input type="text" id="kleur" onChange={this.handleChange} value={this.state.dier.kleur}></input><br></br>                    
+                    <label>Karakter van het dier</label><br></br>
+                    <input type="text" id="karakter" onChange={this.handleChange} value={this.state.dier.karakter}></input><br></br>
+                    <label>Geslacht van het dier</label><br></br>
+                    <input type="text" id="geslacht" onChange={this.handleChange} value={this.state.dier.geslacht}></input><br></br>
+                    <label>Leeftijd van het dier</label><br></br>
+                    <input type="number" id="leeftijd" onChange={this.handleChange} value={this.state.dier.leeftijd}></input><br></br>
+                    <label>Selecteer foto</label><br></br>
+                    <input type="file" id="foto" name="foto" onChange={this.handleChange}></input><br></br>
+                    <input type="submit"></input>
+                </form>
+            </div>
+        );
+    }   
+
 }
 
 export default AddAnimal;
--- Website/finalwork_brammoons_frontend/src/AddWalk.js
+++ Website/finalwork_brammoons_frontend/src/AddWalk.js
@@ -1,18 +1,91 @@
 import React from 'react';
 import axios from 'axios';
 
-function AddWalk() {
-    return(
-        <div id="AddWalk">
-            <h1>Function Add walk test page</h1>
-            <Test />
-        </div>
-        
-    );
-}
+class AddWalk extends React.Component {
 
-function Test() {
-    return <h2>Dit is een function test</h2>
+    constructor(){
+
+        let walk = {
+            "lengte": 5,
+            "startuur": 10,
+            "stopuur": 16,
+            "straat": "Schavolliestraat",
+            "straatnr" : 44,
+            "postcode": 1755,
+            "gemeente": "Gooik",
+            "datum": 2020-6-20
+        };
+
+        let dier;
+        this.state = { walk, dier };
+        
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit() {
+        axios.post("http://127.0.0.1:8080/Wandeling/voegToe?wandeling=" + this.state.walk);
+    }
+
+    handleChange(event) {
+        let walk = this.state.walk;
+        switch(event.target.id){
+            case "lengte":
+                walk.lengte = event.target.value;
+                break;
+            case "startuur":
+                walk.startuur = event.target.value;
+                break;
+            case "stopuur":
+                walk.stopuur = event.target.value;
+                break;
+            case "straat":
+                walk.straat = event.target.value;
+                break;
+            case "straatnr":
+                walk.straatnr = event.target.value;
+                break;
+            case "postcode":
+                walk.postcode = event.target.value;
+                break;
+            case "gemeente":
+                walk.gemeente = event.target.value;
+                break;
+            default:
+                //can be ignored
+        }
+
+        this.setState(walk);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Wandeling toevoegen</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Lengte van de wandeling</label><br></br>
+                    <input type="text" id="lengte" onChange={this.handleChange} value={this.state.walk.lengte}></input><br></br>
+                    <label>Startuur</label><br></br>
+                    <input type="number" id="startuur" onChange={this.handleChange} value={this.state.walk.startuur}></input><br></br>
+                    <label>Stopuur</label><br></br>
+                    <input type="number" id="stopuur" onChange={this.handleChange} value={this.state.walk.stopuur}></input><br></br>                    
+                    <label>Straat</label><br></br>
+                    <input type="text" id="straat" onChange={this.handleChange} value={this.state.walk.straat}></input><br></br>
+                    <label>Straatnummer</label><br></br>
+                    <input type="number" id="straatnr" onChange={this.handleChange} value={this.state.walk.straatnr}></input><br></br>
+                    <label>Postcode</label><br></br>
+                    <input type="number" id="postcode" onChange={this.handleChange} value={this.state.walk.postcode}></input><br></br>
+                    <label>Gemeente</label><br></br>
+                    <input type="text" id="gemeente" onChange={this.handleChange} value={this.state.walk.gemeente}></input><br></br>
+                    <label>Datum</label><br></br>
+                    <input type="date" id="datum" onChange={this.handleChange} value={this.state.walk.datum}></input><br></br>
+                    <input type="submit"></input>
+                </form>
+            </div>
+            
+        );
+    }
+
 }
 
 export default AddWalk;
